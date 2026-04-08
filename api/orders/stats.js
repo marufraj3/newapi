299da0465e05a4c3c557d2f7f72fcd859cc34ac8
MD. Maruf Hasan <marufhasan03938@gmail.com>
2026-04-08 19:55:22 +0600

    Update stats.js

--- api/orders/stats.js
+++ api/orders/stats.js
@@ -9,16 +9,15 @@ import {
   groupByStatus,
 } from "../../lib/bulkprovider.js";
 
-if (req.method === 'OPTIONS') {
+res.setHeader('Access-Control-Allow-Origin', '*');
+
+export default async function handler(req, res) {
+  if (req.method === 'OPTIONS') {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Api-Key');
   return res.status(200).end();
 }
-
-res.setHeader('Access-Control-Allow-Origin', '*');
-
-export default async function handler(req, res) {
   if (req.method === "OPTIONS") return res.status(200).end();
   if (req.method !== "GET") {
     return res.status(405).json({ error: "Method not allowed. Use GET." });
